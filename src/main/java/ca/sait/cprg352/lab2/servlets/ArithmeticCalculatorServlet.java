e7c820e28c98d59a2a450daa8265f1c0326b2199
Lisa Jowett <lisajowett@live.ca>
2022-01-19 18:36:52 -0700

    Part 2 initial commit

---
+++ src/main/java/ca/sait/cprg352/lab2/servlets/ArithmeticCalculatorServlet.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package ca.sait.cprg352.lab2.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author lisaj
+ */
+@WebServlet(name = "ArithmeticCalculatorServlet", urlPatterns = {"/ArithmeticCalculatorServlet"})
+public class ArithmeticCalculatorServlet extends HttpServlet 
+{
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        String inputOne = request.getParameter("input1");
+        String inputTwo = request.getParameter("input2");
+        
+        String operand = request.getParameter("button");
+
+        if(inputOne != null && inputTwo != null)
+            {
+                try
+                    {
+                        if (operand.equals("+"))
+                            {
+                                int input1 = Integer.parseInt(inputOne);
+                                int input2 = Integer.parseInt(inputTwo);
+                                int sum = input1 + input2;
+                                request.setAttribute("message", "Result: " + sum);
+                            }
+
+                        if (operand.equals("-"))
+                            {
+                                int input1 = Integer.parseInt(inputOne);
+                                int input2 = Integer.parseInt(inputTwo);
+                                int minus = input1 - input2;
+                                request.setAttribute("message", "Result: " + minus);
+                            }
+                        if (operand.equals("*"))
+                            {
+                                int input1 = Integer.parseInt(inputOne);
+                                int input2 = Integer.parseInt(inputTwo);
+                                int multiply = input1 * input2;
+                                request.setAttribute("message", "Result: " + multiply);
+                            }
+                        if (operand.equals("%"))
+                            {
+                                int input1 = Integer.parseInt(inputOne);
+                                int input2 = Integer.parseInt(inputTwo);
+                                int divide = input1 % input2;
+                                request.setAttribute("message", "Result: " + divide);
+                            }
+                    }
+
+                catch(Exception e)
+                    {
+                        request.setAttribute("message", "Result: Invalid");
+                    }
+            }
+
+                else
+                    {
+                        request.setAttribute("message", "Result: Invalid");
+                    }   
+    }
+}
+
+ 
---
+++ src/main/webapp/WEB-INF/arithmeticcalculator.jsp
@@ -0,0 +1,36 @@
+<%-- 
+    Document   : arithmeticcalculator
+    Created on : 18-Jan-2022, 6:52:24 PM
+    Author     : lisaj
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Arithmetic Calculator</title>
+    </head>
+    <body>
+        <h1>Arithmetic Calculator</h1>
+        
+        <form action="arithmetic" method="post">
+            
+            <label for="input1">First:</label>
+            <input type="number" name="input1" id="input1">
+            <br>
+            <label for="input2">Second:</label>
+            <input type="number" name="input2" id="input2">
+            <br>
+            <input type="submit" name="button" value="+">
+            <input type="submit" name="button" value="-">
+            <input type="submit" name="button" value="*">
+            <input type="submit" name="button" value="%">
+        
+        </form>
+        
+        <p>$(message)</p>
+        <br>
+        <a href="age">Age Calculator Page</a>
+    </body>
+</html>
